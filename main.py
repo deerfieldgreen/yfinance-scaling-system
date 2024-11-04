906bed392b09ffde4407a6d4e9b720c0e29228b4
Abhishek <abhi555shek@gmail.com>
2024-11-04 19:26:42 +0530

    Temp exit

--- main.py
+++ main.py
@@ -5,21 +5,17 @@ from utils.stock_splits import generate_stock_splits
 from utils.github_utils import push_to_github
 
 def main():
- 
+
+  exit(0) # temporary to stop endless loop
 
   ## All functions push their data to the ../data/ directory
   generate_stock_splits()
   ## Add other modules here
 
 
-
   ## Then all files in data get committ-pushed
   ## Push to GitHub comes last
   push_to_github()
-
-
-
-
 
 
 if __name__ == '__main__':
